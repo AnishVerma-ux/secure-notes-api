62339df85d5170e9bb2387b376100f66921a7128
anishverma <anishverma631@gmail.com>
2026-07-01 11:07:41 +0530

    import mail sender

--- secure-notes-api/src/main/java/com/anish/secure_notes_api/service/EmailService.java
+++ secure-notes-api/src/main/java/com/anish/secure_notes_api/service/EmailService.java
@@ -5,7 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 @Service
 public class EmailService {
     private final Logger logger = LoggerFactory.getLogger(EmailService.class);
